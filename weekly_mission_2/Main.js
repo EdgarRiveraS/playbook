d1b7ca81e78dfcd3a3a716c4b405c38f436bc91a
Edgar Rivera <launchx08831@innovaccion.mx>
2022-05-01 23:57:43 -0500

    Commit 2da semana ejemplo del 1 al 5

---
+++ weekly_mission_2/Main.js
@@ -0,0 +1,5 @@
+const eje_01 = require('./Ejemplo_1');
+const eje_02 = require('./Ejemplo_2');
+const eje_03 = require('./Ejemplo_3');
+const eje_04 = require('./Ejemplo_4');
+const eje_05 = require('./Ejemplo_5');
