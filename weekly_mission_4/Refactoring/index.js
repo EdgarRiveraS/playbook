45eefe5d1dfa55bc697d2b0acf4f5985b11b56a7
Edgar Rivera <launchx08831@innovaccion.mx>
2022-05-06 21:11:49 -0500

    commit Refactoring terminado

--- weekly_mission_4/Refactoring/index.js
+++ weekly_mission_4/Refactoring/index.js
@@ -3,8 +3,11 @@ const ExplorerService = require('./lib/services/explorerServices')
 const explorers = Reader.readJsonFile('explorers.json')
 const FizzbuzzService = require ('./lib/services/FizzbuzzService')
 
-const explorerByMission = ExplorerService.filterByMission(explorers, "Node");
-const AmountOfExplorersByMission = ExplorerService.getAmountOfExplorersByMission(explorers,"Node");
-const UsernamesByMission = ExplorerService.getExplorersUsernamesByMission(explorers, "Node");
-const izzBuzzAndOtherTrick = FizzbuzzService.applyValidationInExplorer(explorers);
+const explorerByMission = ExplorerService.filterByMission(explorers, "node");
+const AmountOfExplorersByMission = ExplorerService.getAmountOfExplorersByMission(explorers,"node");
+const UsernamesByMission = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+const FizzBuzzAndOtherTrick = FizzbuzzService.applyValidationInExplorer(explorers);
 console.log(explorerByMission);
+console.log(AmountOfExplorersByMission)
+console.log(UsernamesByMission)
+console.log(FizzBuzzAndOtherTrick)
