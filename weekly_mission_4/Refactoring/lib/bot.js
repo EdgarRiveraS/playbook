4f31111bd3651c3d756e0c9bd6e85a6ec45f94e6
Edgar Rivera <launchx08831@innovaccion.mx>
2022-05-13 18:34:38 -0500

    agregando usernames ajolonautas al bot

--- weekly_mission_4/Refactoring/lib/bot.js
+++ weekly_mission_4/Refactoring/lib/bot.js
@@ -1,24 +1,29 @@
-require('dotenv').config();
+require("dotenv").config();
 if(!process.env.TOKENBOT){
-    throw new Error ("No hay configuración con Token")
+    throw new Error("No hay configuracion con token");
 }
 const TelegramBot = require("node-telegram-bot-api");
 const ExplorerController = require("./controllers/ExplorerController");
+
+// replace the value below with the Telegram token you receive from @BotFather
 const token = process.env.TOKENBOT;
-console.log(token);
+
+// Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
+
 // Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, (msg, match) => {
-    // 'msg' is the received Message from Telegra
+    // 'msg' is the received Message from Telegram
     // 'match' is the result of executing the regexp above on the text content
-    // of the message    
+    // of the message
+
     const chatId = msg.chat.id;
     const resp = match[1]; // the captured "whatever"
 
     // send back the matched "whatever" to the chat
     bot.sendMessage(chatId, resp);
 });
-    
+
 // Listen for any kind of message. There are different kinds of
 // messages.
 bot.on("message", (msg) => {
@@ -26,10 +31,17 @@ bot.on("message", (msg) => {
     const numberToApplyFb = parseInt(msg.text);
 
     if(!isNaN(numberToApplyFb)){
-        const fizzbuzzTrick = ExplorerController.applyFizzbuzz(numberToApplyFb);
+        const fizzbuzzTrick = ExplorerController.applyValidaScoreExplorer(numberToApplyFb);
         const responseBot = `Tu número es: ${numberToApplyFb}. Validación: ${fizzbuzzTrick}`;
         bot.sendMessage(chatId, responseBot);
-    } else {
-        bot.sendMessage(chatId, "Envía un número válido");
-    }    
-});    
+        console.log(msg.text);
+    }else{
+        const resp = ExplorerController.getExplorersUsernamesByMission(msg.text);
+        console.log(resp);
+        if (resp != ""){
+            resp.forEach(explorer => bot.sendMessage(chatId, explorer));
+        }else {
+            bot.sendMessage(chatId, "Número o mission no valida");
+        }
+    }        
+});
